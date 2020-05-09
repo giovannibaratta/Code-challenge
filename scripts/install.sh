edf1e4995b3d8a5876fcdcb16f67ad9d46c21785
Giovanni Baratta <bargiovi@hotmail.it>
2020-05-09 22:00:56 +0200

    force python3

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,9 @@ fi
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
 sudo rm /etc/apt/sources.list.d/ansible*
 
 cd "$ROOT_PATH/ansible"
 ansible --version
-ansible-playbook "configure_building_environment.yml"
+ansible-playbook "configure_building_environment.yml" -e ansible_python_interpreter=$( which python3 )
