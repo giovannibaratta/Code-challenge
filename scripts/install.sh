917ed0eedfc49f88ee5c19067d14d9080b86935d
Giovanni Baratta <gio@pop-os.localdomain>
2020-05-09 18:33:49 +0200

    remove ansible apt key

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,8 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-
-ansible --version
+sudo rm /etc/apt/sources.list.d/ansible*
 
 cd "$ROOT_PATH/ansible"
+ansible --version
 ansible-playbook "configure_building_environment.yml"
