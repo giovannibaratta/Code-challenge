b21eff00f45573a0396aaf27cf528ddc5624454a
Giovanni Baratta <gio@pop-os.localdomain>
2020-05-13 15:31:06 +0200

    fix deployment

---
+++ aws/scripts/deploy_all.sh
@@ -0,0 +1,11 @@
+# Verifica se ROOT_PATH è settato e non vuoto
+if [ ! ${ROOT_PATH:-} ]
+then 
+    echo "ROOT_PATH not set"
+    exit 1
+fi
+
+$ROOT_PATH/aws/scripts/deploy_network.sh && \
+      $ROOT_PATH/aws/scripts/deploy_ec2.sh && \
+      $ROOT_PATH/aws/scripts/configure_ansible_controller.sh && \
+      $ROOT_PATH/aws/scripts/launch_docker_configuration.sh
