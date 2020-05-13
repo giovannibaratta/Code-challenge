a430c5f4f9d1ef60a488a73976757b358ea50587
Giovanni Baratta <gio@pop-os.localdomain>
2020-05-13 16:57:19 +0200

    prevent failure of configuration while ansible_controller is booting up.

--- aws/scripts/configure_ansible_controller.sh
+++ aws/scripts/configure_ansible_controller.sh
@@ -5,6 +5,9 @@ then
     exit 1
 fi
 
+RETRIES=5
+SLEEP_TIME=60
+
 CI_SCRIPT_DIR="$ROOT_PATH/ci/scripts"
 VIRTUAL_ENV_DIR=$( $CI_SCRIPT_DIR/retrieve_virtual_environment_dir.sh )
 echo "VIRTUAL_ENV_DIR -> $VIRTUAL_ENV_DIR"
@@ -12,10 +15,20 @@ echo "VIRTUAL_ENV_DIR -> $VIRTUAL_ENV_DIR"
 . "$VIRTUAL_ENV_DIR/bin/activate"
 cd "$ROOT_PATH/aws/ansible"
 
-ansible-playbook -i inventories/ansible_controller_aws_ec2.yml \
-    configure_ansible_controller.yml \
-    --private-key "$ROOT_PATH/aws/ansible/files/ssh_keys/ansible_controller.pem"
+for INDEX in $( seq 1 $RETRIES ); do
 
+    ansible-playbook -i inventories/ansible_controller_aws_ec2.yml \
+        configure_ansible_controller.yml \
+        --private-key "$ROOT_PATH/aws/ansible/files/ssh_keys/ansible_controller.pem"
+
+    if [ "$?" -eq 0 ]
+    then
+        break
+    fi
+
+    echo "retry in $SLEEP_TIME seconds."
+
+done
 
 ANSIBLE_PLAYBOOK_EXIT_CODE=$?
 deactivate
